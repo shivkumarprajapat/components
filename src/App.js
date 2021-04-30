6107e057f2352b243a2bdbb9fa201759d23f1292
Your Name <you@example.com>
2021-04-30 13:20:35 -0700

    Progress Bar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProgressBar from './Progress-Bar/ProgressBar'
+function App() {
+  return (
+    <>
+      <ProgressBar />
+    </>
+
+  );
+}
+
+export default App;
